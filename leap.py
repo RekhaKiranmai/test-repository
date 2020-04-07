d45582cd5cadefdd584ba5100b0279aeb201bab4
REKHA <mrekhakiranmai@gmail.com>
2020-04-07 11:30:56 +0530

    first version of leap code

---
+++ leap.py
@@ -0,0 +1,12 @@
+n=int(input())
+if n%4==0:
+   if n%100==0:
+      if n%400==0:
+         print("leap")
+      else:
+         print("not leap")
+   else:
+      print("leap")
+         
+else:
+   print("non-leap")
